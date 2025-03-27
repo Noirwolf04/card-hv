d9790e047b8c02066f4710f17ea803278a60c685
Noirwolf04 <nruizb2003@gmail.com>
2025-03-26 23:28:27 -0500

    Se finaliza proyecto

--- app.js
+++ app.js
@@ -1,30 +1,76 @@
 // Ejemplo de data de las cartas
 const cardsData = [
     {
+        frontTitle: "SOBRE MI",
+        frontContent:  [],
+        backTitle: "",
+        backContent: "Soy Nicolas Ruiz, apasionado por la inteligencia artificial y la innovación. Me especializo en la aplicación de IA para potenciar la creatividad, optimizar procesos y diseñando experiencias interactivas que fomentan el aprendizaje y la imaginación."
+      },
+      {
+        frontTitle: "FORMACION ACADEMICA",
+        frontContent:  [],
+        backTitle: "",
+        backContent: ["Gestor de Emprendimiento e Innovación - Universidad del Valle (2023 - 2027)","Desarrollo de Software - Universidad del Valle (2022 - 2023)","Técnico en Asesoría Comercial y Operaciones Financieras - SENA (2021 - 2022)"]
+      },
+      {
+        frontTitle: "FORMACION COMPLEMENTARIA",
+        frontContent:  [],
+        backTitle: "",
+        backContent: ["Data Science - Oracle (2025-2026)","IBM AI Developer - IBM (2024-2025)","Cómo resolver problemas y tomar decisiones con eficacia - University of California, Irvine(2024)","Análisis y Visualización de Datos Avanzada - Talento Tech Mintic (2024)"]
+      },
+    {
         frontTitle: "EXPERIENCIA LABORAL",
         frontContent:  [],
         backTitle: "",
         backContent: ["Banco de occidente", "Mexicaps", "Freelance"]
       },
     {
-        frontTitle: "Banco de Occidente - 2021-2022",
-        frontContent:  ["Cajero", "Auxiliar Operativo", "Asesor Comercial"],
-        backTitle: "Logros",
-        backContent: "Atención al cliente, supervisión, ventas, etc."
+        frontTitle: "IT Project Manager - Mexicaps (Nov 2024 - Feb 2025)",
+        frontContent: " ",
+        backTitle: "Labores y logros",
+        backContent: ["Implementación de un agente AI omnicanal de atencion al cliente", "Automatización de procesos operativos", "Capacitacion en uso de herramientas de inteligencia artifical para procesos creativos y operativos","Estos proyectos permitieron a la empresa aumentar su productividad logrando atender 100% mas clientes mientras se liberan horas de trabajo, y mejorar sus procesos creativos y estrategias de marketing, aumentando el engagement y porcentaje de conversion"]
       },
     {
-      frontTitle: "Proyecto Freelance",
-      frontContent: "Marketing digital y automatización",
-      backTitle: "Detalles",
-      backContent: "Diseño de campañas, chatbots, etc."
+      frontTitle: "Consultor de Marketing Digital - Freelance (Feb 2023 - Oct 2024)",
+      frontContent: "",
+      backTitle: "Labores y logros",
+      backContent: ["Diseño de campañas de Meta Ads efectivas", "Creacion de embudos de venta", "Optimización de campañas de Meta Ads", "Estas estrategias permitieron a los clientes aumentar la visibilidad de sus redes sociales un 500%, triplicar su facturacion y construir una comunidad solida y base de datos calificada"]
     },
     {
-      frontTitle: "Formación",
-      frontContent: "Universidad del Valle",
-      backTitle: "Estudios",
-      backContent: "Gestor de Emprendimiento e Innovación, etc."
-    },
-    // ... Agrega más cartas según necesites
+
+        frontTitle: "Banco de Occidente - (Sep 2021-  Nov 2022)",
+        frontContent:  ["Cajero", "Auxiliar Operativo", "Asesor Comercial"],
+        backTitle: "Labores y logros",
+        backContent: ["Atención al cliente", "supervisión de operaciones", "ventas consultivas", "Operaciones de moneda local y extranjera", "Contabilidades", "Capacitacion","En el desarrollo de mis labores logre aumentar el indicar de satisfacción del cliente en un promedio de 40%, mejorar la calidad de los procesos operativos mediante la implementacion del proyecto de digitalizacion de procesos y traslado de operaciones a la nube"]
+      },
+      {
+
+        frontTitle: "Soft Skills",
+        frontContent: [ "Pensamiento estratégico", "Escucha activa","Adaptabilidad", "Gestion de estes","Pensamiento critico", "Trabajo en equipo","Gestion del cambio"],
+        backTitle: "Hard Skills",
+        backContent: ["Inteligencia Artificial aplicada", "Automatización de procesos", "Manipulacion de datos","Razonamiento analitico", "Diseño de experienciasde usuario", "Marketing Digital"],
+      },
+      {
+
+        frontTitle: "Tecnologias",
+        frontContent: "",
+        backTitle: "",
+        backContent: ["LLMs (Gpt, Claude, Gemini, Foocus,IBM Watson...", "Python", "Html", "Css", "Javascript", "Figma"],
+      },
+      {
+
+        frontTitle: "Tipo Personalidad",
+        frontContent: "INTJ-T",
+        backTitle: "Arquitecto",
+        backContent: ["Introvertido – 60%" , "Intuitivo – 93%" , "Pensamiento – 81%" , "Juzgador – 82%", "Cauteloso – 51%"],
+      },
+      {
+
+        frontTitle: "Gracias por leer mi informacion",
+        frontContent: "Si llegaste hasta aquí, te agradezco por tu tiempo. Asumo que mi perfil te interesó y crees que podria encajar en la vacante, así que en la tarjeta siguiente te dejo información mi informacion de contacto y mi linkedin si quiere saber mas de mi.",
+        backTitle: "Contáctame",
+        backContent: ["Email: nicolas.bolanos@correounivalle.edu.co", "Linkedin: https://www.linkedin.com/in/nicolas-ruiz-953323302/", "Teléfono: 302 868 2222"],
+      },
   ];
   
   // Índice de la carta actual
@@ -107,7 +153,7 @@ function updateCardContent() {
       const ul = document.createElement('ul');
       content.forEach(item => {
         const li = document.createElement('li');
-        li.textContent = item;
+        li.textContent = "• " + item;
         ul.appendChild(li);
       });
       container.appendChild(ul);
@@ -154,7 +200,7 @@ function updateCardContent() {
         flipCard();
       }
     } else {
-      alert("¡No hay más cartas!");
+      alert("¡Abajo tienes mi informacion de contacto, no la pierdas!");
     }
   }
   
@@ -168,7 +214,7 @@ function updateCardContent() {
         flipCard();
       }
     } else {
-      alert("¡Esta es la primera carta!");
+      alert("¡Click en el boton **siguiente** para saber mas de mi!");
     }
   }
   
